fcb2b3493f5c4e543194bcd849f9d993f1067a49
Terje Norderhaug <terje@in-progress.com>
2019-06-08 23:11:18 -0700

    Add form for funding requests

--- src/app/view/invoice.cljs
+++ src/app/view/invoice.cljs
@@ -12,13 +12,48 @@
 
 (def requesting-funds (rf/subscribe [:requesting-funds]))
 
+(defn amount-field [{:keys [value]}]
+  [:<>
+   [:> mui/InputLabel
+    {:htmlFor "input-amount"}
+    "Amount"]
+   [:> mui/Input
+    {:id "input-amount"
+     :value @value
+     :fullWidth true
+     :on-change (fn [e]
+                  (reset! value (.. e -target -value)))
+     :endAdornment
+     (reagent/as-element
+      [:> mui/InputAdornment {:position "end"} "SAT"])}]])
+
+(defn memo-field [{:keys [value]}]
+   [:> mui/TextField
+    {:label "Memo"
+     :value @value
+     :fullWidth true
+     :margin "normal"
+     :on-change (fn [e]
+                  (reset! value (.. e -target -value)))}])
+
+(defn hash-field [{:keys [amount memo]}]
+  [:div
+     (str amount "+" memo)]) 
+
+
 (defn funding-request-card []
-  (if @requesting-funds
-    [ui/card
-     [ui/card-header
-      {:title "Funding Request"
-       :subheader "Receive funds through the Lightning Network"
-       :avatar (-> [:> mui/Avatar [:> MoneyIcon]]
-                   reagent/as-element)}]
-     [ui/card-content
-      #_[:div "Requesting Funds"]]]))
+  (let [amount (reagent/atom "")
+        memo (reagent/atom "")]
+    (fn []
+      (if @requesting-funds
+        [ui/card
+         [ui/card-header
+          {:title "Funding Request"
+           :subheader "Receive funds through the Lightning Network"
+           :avatar (-> [:> mui/Avatar [:> MoneyIcon]]
+                       reagent/as-element)}]
+         [ui/card-content
+          [:> mui/FormControl
+           [amount-field {:value amount}]
+           [memo-field {:value memo}]
+           [hash-field {:amount @amount :memo @memo}]]]]))))
