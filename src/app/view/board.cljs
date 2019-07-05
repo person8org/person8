c66b91ddaf92d31577981269ed1c13b7bb40c10c
Terje Norderhaug <terje@in-progress.com>
2019-07-05 14:39:29 -0700

    Use responsive grid layout

--- src/app/view/board.cljs
+++ src/app/view/board.cljs
@@ -3,6 +3,8 @@
     [taoensso.timbre :as timbre]
     ["@material-ui/core" :as mui]
     ["@material-ui/icons/FileCopy" :default CopyIcon]
+    ["@material-ui/core/Grid" :default Grid]
+    ["@material-ui/core/Hidden" :default Hidden]
     [re-frame.core :as rf]
     [reagent.core :as reagent]
     [app.view.invoice :as invoice-view]
@@ -16,24 +18,32 @@
 (defmulti pane (fn [{:keys [stage] :as session}]
                   (if stage [@stage])))
 
-(defmethod pane :default [{:keys [mobile profile] :as session}]
-  [:div {:style {:padding-top "60px"}}
-   (if (= (get-in @mobile [:user :status]) "SUCCESS")
-     (into
-      [:div [identity-card session]]
-      (for [item (:fields @profile)]
-        [profile-card item]))
-    [:div])])
+(defn board-listing [{:keys [items]}]
+   (into
+    [:> mui/List
+     [invoice-view/funding-request-card]]
+    (for [{:keys [selected] :as item} items]
+       [:> mui/ListItem
+        {:selected (boolean selected)}
+        [pane/view {:item item}]])))
 
-(defn board-listing [items]
+(defn board-grid [{:keys [items]}]
   (into
-   [:> mui/List
-    [invoice-view/funding-request-card]]
-   (for [{:keys [selected] :as item} items]
-     [:> mui/ListItem
-      {:selected (boolean selected)}
-      [pane/view {:item item}]])))
+   [:> Grid {:container true :spacing 8}
+     [:> Grid {:item true :xs 12}
+       [invoice-view/funding-request-card]]
+    (for [{:keys [selected] :as item} items]
+      [:> Grid {:item true :xs 12 :sm 6 :md 4}
+        #_{:selected (boolean selected)}
+        [pane/view {:item item}]])]))
 
 (defn board-pane [items]
   (timbre/debug "Board Pane:" items)
-  [board-listing items])
+  [:<>
+   [:> mui/Hidden {:sm-up true}
+    [board-listing {:items items}]]
+   [:> mui/Hidden {:xs-down true}
+    [board-grid {:items items}]]])
+
+(defmethod pane :default [{:keys [mobile profile] :as session}]
+  [board-pane (:fields @profile)])
