614df83d09c8d2b94c1e7c2b956b4aef3642cd8b
Terje Norderhaug <terje@in-progress.com>
2019-07-17 12:55:51 -0700

    Style landing page

--- src/app/view/board.cljs
+++ src/app/view/board.cljs
@@ -73,7 +73,7 @@
           (for [{:keys [id selected] :as item} items]
             ^{:key id}
            [:> Grid {:item true :xs 12 :sm 6 :md 4
-                     :classes {:root ["board-grid-item"]}}
+                     :classes {:root "board-grid-item"}}
              #_{:selected (boolean selected)}
              [error-boundary {}
               [entry/pick-zone {:item item
